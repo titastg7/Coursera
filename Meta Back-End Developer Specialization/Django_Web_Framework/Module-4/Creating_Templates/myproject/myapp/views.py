e302aa0711352e2b8968e6cd2adf4018ae3d3cd4
titastg7 <53432312+titastg7@users.noreply.github.com>
2024-08-19 10:25:37 +0530

    Add files via upload

---
+++ Meta Back-End Developer Specialization/Django_Web_Framework/Module-4/Creating_Templates/myproject/myapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def about(request):
+    about_content = {'about': "Little Lemon is a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist. The chefs draw inspiration from Italian, Greek, and Turkish culture and have a menu of 12–15 items that they rotate seasonally. The restaurant has a rustic and relaxed atmosphere with moderate prices, making it a popular place for a meal any time of the day."} 
+    return render(request, 'about.html',about_content)
+
+def menu(request):
+    return render(request, 'menu.html', {})
