baba71047cc9be3d3e8562645fc8999e053b1970
titastg7 <53432312+titastg7@users.noreply.github.com>
2024-09-02 21:03:17 +0530

    Update urls.py

--- Meta Back-End Developer Specialization/APIs/Module-4/LittleLemon/LittleLemonAPI/urls.py
+++ Meta Back-End Developer Specialization/APIs/Module-4/LittleLemon/LittleLemonAPI/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     path('cart/menu-items/', views.CartView.as_view()),
     path('orders/', views.OrderView.as_view()),
     path('orders/<int:pk>/', views.SingleOrderView.as_view()),
-]
 
+]
