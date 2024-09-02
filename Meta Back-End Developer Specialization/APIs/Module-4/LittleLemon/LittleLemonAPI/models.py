bf51017ed1936742c6ebe4fad5feff30898e7721
titastg7 <53432312+titastg7@users.noreply.github.com>
2024-09-02 09:52:46 +0530

    Update models.py

--- Meta Back-End Developer Specialization/APIs/Module-4/LittleLemon/LittleLemonAPI/models.py
+++ Meta Back-End Developer Specialization/APIs/Module-4/LittleLemon/LittleLemonAPI/models.py
@@ -39,7 +39,7 @@ class Order(models.Model):
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     menuitem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
-    quantity = models.SmallIntegerField(),
+    quantity = models.SmallIntegerField()
     unit_price = models.DecimalField(max_digits=6,decimal_places=2)
     price = models.DecimalField(max_digits=6, decimal_places=2)
 
