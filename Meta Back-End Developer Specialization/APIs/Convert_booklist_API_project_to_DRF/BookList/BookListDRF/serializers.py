74e6a39346e5ef4f8ac73969bb57b20c34c94407
titastg7 <53432312+titastg7@users.noreply.github.com>
2024-09-02 09:43:16 +0530

    Delete Meta Back-End Developer Specialization/APIs/Convert_booklist_API_project_to_DRF/BookList directory

--- Meta Back-End Developer Specialization/APIs/Convert_booklist_API_project_to_DRF/BookList/BookListDRF/serializers.py
+++
@@ -1,7 +0,0 @@
-from .models import Book
-from rest_framework import serializers
-
-class BookSerializer(serializers.ModelSerializer) :
-    class Meta :
-        model = Book
-        fields = ['id','title','author','price']
