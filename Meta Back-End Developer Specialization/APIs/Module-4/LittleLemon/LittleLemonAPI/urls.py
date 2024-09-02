a5bff562d34a7a3c1fa94582d2c7c482add9ca38
titastg7 <53432312+titastg7@users.noreply.github.com>
2024-09-02 16:57:19 +0530

    Update urls.py

--- Meta Back-End Developer Specialization/APIs/Module-4/LittleLemon/LittleLemonAPI/urls.py
+++ Meta Back-End Developer Specialization/APIs/Module-4/LittleLemon/LittleLemonAPI/urls.py
@@ -11,5 +11,7 @@ urlpatterns = [
     path('groups/delivery-crew/users/', views.DeliveryCrewView.as_view()),
     path('groups/delivery-crew/users/<int:pk>/', views.SingleDeliveryCrewView.as_view()),
     path('cart/menu-items/', views.CartView.as_view()),
+    path('orders/', views.OrderView.as_view()),
+    path('orders/<int:pk>/', views.SingleOrderView.as_view()),
 ]
 
